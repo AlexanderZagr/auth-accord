897c73aaf6e2c5d4e6e3498bcfda0b0f552df6e1
AlexanderZagr <auzagraevskiy@gmail.com>
2021-04-27 22:36:48 +0300

    auth-accord

--- src/main/java/com/jwt/auth/model/mySql/Role.java
+++ src/main/java/com/jwt/auth/model/mySql/Role.java
@@ -14,8 +14,21 @@ public class Role {
     @Column(name = "role")
     private String role;
 
+    public int getRoleId() {
+        return roleId;
+    }
 
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     @Override
     public boolean equals(Object o) {
